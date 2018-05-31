de05441a1ed97293ba16808a398d881e1e940fb7
JRobsonJr <jrobsonjr16@gmail.com>
2018-05-31 15:41:48 -0300

    Organize imports.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { loadProgressBar } from 'axios-progress-bar';
-import 'axios-progress-bar/dist/nprogress.css';
-import './style.css';
 
 import App from './components/app/App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'font-awesome/css/font-awesome.min.css';
+import 'axios-progress-bar/dist/nprogress.css';
+import './style.css';
 
 loadProgressBar();
 
